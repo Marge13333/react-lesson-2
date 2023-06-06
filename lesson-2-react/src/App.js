6d45ca9ce8412186c7b2f6eb2e563bbe9479a8ce
Marge13333 <113912686+Marge13333@users.noreply.github.com>
2023-06-06 18:41:58 +0400

    idk

--- lesson-2-react/src/App.js
+++ lesson-2-react/src/App.js
@@ -1,8 +1,72 @@
-import Player from './player/player';
+import React from "react";
+import axios from 'axios';
 
-function App() {
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { useState } from 'react';
+
+
+function App(e) {
+  const [search,setSearch] = useState()
+  const[product, setProduct] = useState([])
+
+  const SearchClick = async (e) =>{
+   try{
+    e.preventDefault() 
+
+    const response = await axios.get(
+      `https://dummyjson.com/products/${search}`,
+      
+    )
+
+const prod = response
+
+    setProduct(prod)
+
+    console.log(prod)
+
+  }catch{
+
+}
+  }
+
+
   return (
-    <Player/>
+    
+      <BrowserRouter>
+      
+       <Route exact path="/" >
+       <div className="inputDiv">  
+       <form >
+       <input className='searchInput' type="number" placeholder="Type number" value={search} onChange={(e) => setSearch(e.target.value)}/> 
+            <Link to={`/product/${search}` }>
+            <button onClick={SearchClick}  className='searchBtn' type="submit" >
+              search
+              </button>     
+              </Link> 
+        </form> 
+       </div> 
+       </Route>
+
+
+       <Route exact path="/product/:search" >
+          
+       <h1>{product.price}</h1>
+       </Route>
+
+
+
+    </BrowserRouter>
+
+
+   
+
+
+
   );
 }
 
