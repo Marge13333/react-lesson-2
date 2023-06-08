b1ac0145b32a2054abe979a5d67c7e626d0e7d24
Marge13333 <113912686+Marge13333@users.noreply.github.com>
2023-06-09 02:05:35 +0400

    idk

--- lesson-2-react/src/App.js
+++ lesson-2-react/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from 'axios';
-
+import './style.css'
 import {
   BrowserRouter,
   Switch,
@@ -10,13 +10,17 @@ import {
 import { useState } from 'react';
 
 
-function App(e) {
+function App() {
   const [search,setSearch] = useState()
   const[product, setProduct] = useState([])
+  const [error,setError] = useState('')
+  const [photoRemove,setPhotoremove] = useState("photo")
 
   const SearchClick = async (e) =>{
    try{
     e.preventDefault() 
+    setError('')
+    setPhotoremove('photo')
 
     const response = await axios.get(
       `https://dummyjson.com/products/${search}`,
@@ -25,37 +29,48 @@ function App(e) {
 
 const prod = response
 
-    setProduct(prod)
+    setProduct(prod.data)
 
     console.log(prod)
 
   }catch{
-
+    setError('პროდუქტი ვერ მოიძებნა')
+    setProduct("")
+    setPhotoremove('removePhoto')
 }
   }
-
 
   return (
     
       <BrowserRouter>
       
        <Route exact path="/" >
-       <div className="inputDiv">  
-       <form >
+        <div className="searchWrapper">
+        <div className="inputDiv">  
        <input className='searchInput' type="number" placeholder="Type number" value={search} onChange={(e) => setSearch(e.target.value)}/> 
-            <Link to={`/product/${search}` }>
-            <button onClick={SearchClick}  className='searchBtn' type="submit" >
-              search
-              </button>     
+              <button onClick={SearchClick}  className='searchBtn' type="button" >
+              <Link to={`/product/${search}` }>
+             search
               </Link> 
-        </form> 
+              </button>   
        </div> 
+        </div>
+  
        </Route>
 
 
        <Route exact path="/product/:search" >
-          
-       <h1>{product.price}</h1>
+        <div className="wrapper">
+        <div className="card">
+       <img  className={photoRemove} src={product.thumbnail} alt='idk'/>
+       <h1 className="title">{product.title}</h1>
+       <p className="productDescr">{product.description}</p>
+       <p className="price">{product.price}</p>
+      <h1>{error}</h1>
+       </div>
+      <Link className="button" to={`/`} >Back</Link>
+        </div>
+        
        </Route>
 
 
